bd6f6414a8ddde8e27eb363069dccd0668392271
alixndra0 <alexescurell@gmail.com>
2024-10-10 16:53:01 +0800

    update login form

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\MainProductController;
 use App\Http\Controllers\PharmacyController;
